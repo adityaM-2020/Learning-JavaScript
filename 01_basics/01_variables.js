19beece271243b5fc9c4504dbe1a044dd5e6cfdd
Aditya <104820673+adityaM-2020@users.noreply.github.com>
2024-01-18 18:34:30 +0000

    variations of variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,8 @@
+//'var' can be redeclared and reassigned
 var a=2;
 var a=11; console.log(a);
 a = 17; console.log(a); 
-/*'var' can be redeclared and reassigned ,Avoid using 'var'*/
+/*Prefer not to use 'var' because of issue in block scope and functional scope*/
 
 let b = 100;
 console.log(b);
@@ -14,5 +15,8 @@ const d=200;
 console.log(d);
 //const d=201; -> 'const'can not be redeclared 
 //d = 202; -> 'const' can not be reassigned 
+
+e=300; //not a good practice but it will work fine
+console.log(e);
 
 
