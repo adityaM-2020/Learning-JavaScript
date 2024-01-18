f943dce070ebabc4725d79284aae207e08c67fd3
Aditya <104820673+adityaM-2020@users.noreply.github.com>
2024-01-18 18:24:17 +0000

    variation of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+var a=2;
+var a=11; console.log(a);
+a = 17; console.log(a); 
+/*'var' can be redeclared and reassigned ,Avoid using 'var'*/
+
+let b = 100;
+console.log(b);
+// let b = 101; ->'let' can not be redeclared
+b = 102;//'let' can be reassigned
+console.log(b);
+
+//const c; ->initialization of 'const' is must at time of its declaration
+const d=200;
+console.log(d);
+//const d=201; -> 'const'can not be redeclared 
+//d = 202; -> 'const' can not be reassigned 
+
+
