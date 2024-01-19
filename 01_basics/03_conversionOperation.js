0c1228fce1e1f9a4817d4e0c5916b464b851ecd5
Aditya <104820673+adityaM-2020@users.noreply.github.com>
2024-01-19 07:57:31 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,23 +14,23 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = "Aditya";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 //  console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+let someNumber = 33;
 
-let stringNumber = String(someNumber)
+let stringNumber = String(someNumber);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
-let value = 3
-let negValue = -value
+let value = 3;
+let negValue = -value ;
 // console.log(negValue);
 
 // console.log(2+2); => 4
@@ -40,10 +40,10 @@ let negValue = -value
 // console.log(2/3); => 0.66666
 // console.log(2%3); => 2
 
-let str1 = "hello"
-let str2 = " hitesh"
+let str1 = "hello";
+let str2 = " Aditya";
 
-let str3 = str1 + str2
+let str3 = str1 + str2;
 // console.log(str3);
 
 // console.log("1" + 2); => 12 (string)
@@ -55,9 +55,9 @@ let str3 = str1 + str2
 // console.log(+true); => 1 
 // console.log(+""); => 0 
 
-let num1, num2, num3
+let num1, num2, num3;
 
-num1 = num2 = num3 = 2 + 2 //poor code readability , Don't use such type of practices
+num1 = num2 = num3 = 2 + 2; //poor code readability , Don't use such type of practices
 
 let gameCounter = 100, gamePostfix = 200;
 ++gameCounter; //prefix operation
