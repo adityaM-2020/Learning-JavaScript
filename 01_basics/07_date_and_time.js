f3ca1f054efa5ff2aebbd942b517c7cc4e4015c8
Aditya <104820673+adityaM-2020@users.noreply.github.com>
2024-01-23 00:35:33 +0530

    date in js

--- 01_basics/07_date_and_time.js
+++ 01_basics/07_date_and_time.js
@@ -23,7 +23,6 @@ let myTimeStamp = Date.now()
 let myCreatedDate = new Date("01-14-2023")
 //console.log(myCreatedDate.toLocaleString());
 // console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date();
 // console.log(newDate);
