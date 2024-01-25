da0389937a62bc6cab39eee1803c7a833351b27b
Aditya <104820673+adityaM-2020@users.noreply.github.com>
2024-01-25 18:08:34 +0000

    control flow in js

---
+++ 04_controlflow.js/02_switch.js
@@ -0,0 +1,36 @@
+/*
+switch (key) {
+    case value:
+        // Statements
+        break;
+
+    case value:
+        // Statements
+        break;
+
+    default:
+        // Statements
+        break;
+}
+*/
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
